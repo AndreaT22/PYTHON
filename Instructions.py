658d04fbed21f9dcefb7343b648a6e99e94bf3c7
AndreaT22 <andrea.toth22@yahoo.com>
2023-05-23 20:53:17 +0200

    managed to do it

---
+++ Instructions.py
@@ -0,0 +1,27 @@
+# open the bag
+# get the bread out of the bag
+# open the drawer
+# get the knife out of the drawer
+# close the drawer
+# cut two slices with the knife
+# walk to the fridge
+# get the butter and the ham out of the fridge
+# close the fridge
+# walk back to the bread with the butter and the ham
+# put the butter on one of the slices and spread it with the knife
+# put the ham on top of it
+# put the other slice on top of the ham
+# stop and sit down
+
+# Animal quiz (3 questions, 2 points for each correct answer, 1 minus if the answer is incorrect)
+# ask a question
+# give three options to choose from
+# get the input from the reader
+# indicate if it's correct or not
+# mark the points: 
+    # if: correct, give 2 points 
+    # else: it's incorrect, give -1 points
+# ask the next question
+# give three options to choose from
+# wait for the reader to choose one answer
+
